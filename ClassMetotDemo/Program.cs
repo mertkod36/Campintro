46b5e7180e81c2b8c33a744ca03282f45a69ec04
Mertcan Karadağ <mertcan.karadag@ogr.deu.edu.tr>
2023-10-30 06:54:57 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ClassMetotDemo;
+
+Musteri musteri1 = new Musteri();
+musteri1.Adi ="ali" ;
+musteri1.Soyadi ="tas";
+musteri1.Id = 1 ;
+
+Musteri musteri2 = new Musteri();
+musteri2.Adi = "ayşe";
+musteri2.Soyadi = "ileri";
+musteri2.Id = 2;
+
+
+
+
+
+
+
+
