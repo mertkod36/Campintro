f8c482735a47e40b6df08857c85f8c0dc45785c2
Mertcan Karadağ <mertcan.karadag@ogr.deu.edu.tr>
2023-11-03 21:09:35 +0300

    ders4

---
+++ OOP2/ProductManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    internal class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine( product.ProductName+"eklendi");
+
+        }
+
+        public void upDate(Product product)
+        {
+            Console.WriteLine(product.ProductName+"güncellendi");
+
+        }
+
+        public int Topla(int sayi1,int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(  sayi1+sayi2);
+
+        }
+
+
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using OOP2;
+
+Product product1= new Product();
+product1.Id = 1;
+product1.CategoryId = 4;
+product1.ProductName ="kalem";
+product1.UnitPrice = 24;
+product1.UnitInStock =82;
+
+
+Product product2= new Product { Id=2,CategoryId=9,ProductName="tablet",UnitPrice=3000,UnitInStock=44}; 
+
+
+//PascalCase   //camelCase
+//case sensitive
+ProductManager productManager = new ProductManager();
+productManager.Add(product1);
+Console.WriteLine(  product1.ProductName);
+
+Console.WriteLine("---------------------");
+
+int toplamSonuc=productManager.Topla(2,3);
+Console.WriteLine(  "----");
+
+productManager.Topla2(2, 3);
